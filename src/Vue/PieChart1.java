12a575f54a864525af4bd1701bd6f02326b14b43
Vgiot14 <vgiot14@gmail.com>
2018-04-09 09:41:34 +0200

    ajout classe PieChart1

---
+++ src/Vue/PieChart1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vue;
+
+import java.awt.Font;
+
+import javax.swing.JPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
+
+
+/**
+ *
+ * @author valentingiot
+ */
+public class PieChart1 {
+    
+}
